bc512afe1e60fb15940200f9e93046ff117d80ff
AmrithaPG <121300743+AmrithaPG@users.noreply.github.com>
2022-12-23 20:35:08 +0530

    Add files via upload

---
+++ Assignment 2 Task 2.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+list = [12,-7,5,64,-14]
+for i in list :
+    if (i>0):
+        print("Output :  ",i)
+
+
+# In[9]:
+
+
+list = [12,14,-95,3]
+for i in list :
+    if (i>0):
+        print("Output :  ",i)
+
+
+# In[ ]:
+
+
+
+
