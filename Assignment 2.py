c1136994343aa352957efcf3c61ff2a847f99158
AmrithaPG <121300743+AmrithaPG@users.noreply.github.com>
2022-12-23 20:10:29 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=int(input("no. of terms:"))
+x=0
+y=1
+if a<=0:
+    print(x)
+else:
+    print(x,y,end=' ')
+    for i in range(2,a):
+        z=x+y
+        print(z,end=' ')
+        x=y
+        y=z
+
+
+# In[ ]:
+
+
+
+
