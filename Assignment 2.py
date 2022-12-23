cd4b471e02af8c087cc49f3a37737b64824e4a5f
AmrithaPG <121300743+AmrithaPG@users.noreply.github.com>
2022-12-23 20:16:18 +0530

    Add files via upload

---
+++ Assignment 2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[6]:
+
+
+x=0
+y=1
+if a<=0:
+    print(x)
+else:
+    print(x,y,end=' ')
+    for i in range(2,a):
+        z=x+y
+        print(z,end=' ')
+        x=y
+        y=z
+
+
+# In[ ]:
+
+
+
+
