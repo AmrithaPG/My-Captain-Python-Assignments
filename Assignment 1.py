69c66dfe44f6d8a5d5ebace8660db8686e1ad0c5
AmrithaPG <121300743+AmrithaPG@users.noreply.github.com>
2022-12-23 20:01:47 +0530

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+Task 1 
+# In[2]:
+
+
+pi=3.141592654
+r=float(input("The radius of a circle:"))
+area=pi*r**2
+print("The area of the circle with radius is:" ,area)
+
+Task 2
+# In[8]:
+
+
+f=(input("Filename: "))
+f_n = f.split (".")
+print("The extension of the file is : " + f_n[-1])
+
+
+# In[ ]:
+
+
+
+
